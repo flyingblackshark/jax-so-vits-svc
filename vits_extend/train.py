cfb70b2a6ee5017499b352b87ddac1c335f96eae
FBS <aliu2000@outlook.com>
2023-06-09 14:35:28 +0800

    testing

--- vits_extend/train.py
+++ vits_extend/train.py
@@ -124,7 +124,7 @@ def train(rank, args, chkpt_path, hp, hp_str):
             loss_kl_f = jnp.mean(loss_kl_f)
             loss_kl_r = jnp.mean(loss_kl_r)
             # Loss
-            loss_g = score_loss + feat_loss + mel_loss + stft_loss + loss_kl_f + loss_kl_r * 0.5# + spk_loss * 0.5
+            loss_g = score_loss + feat_loss + mel_loss + stft_loss #+ loss_kl_f + loss_kl_r * 0.5# + spk_loss * 0.5
 
             return loss_g
 
