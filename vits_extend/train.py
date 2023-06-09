c085c1a31cba6240866ad27a63eee710e1d0ba0a
FBS <aliu2000@outlook.com>
2023-06-09 13:23:36 +0800

    Testing

--- vits_extend/train.py
+++ vits_extend/train.py
@@ -259,8 +259,8 @@ def train(rank, args, chkpt_path, hp, hp_str):
 
             step += 1
             # logging
-            loss_g = generator_loss#loss_g.item()
-            loss_d = discriminator_loss#loss_d.item()
+            #loss_g = generator_loss#loss_g.item()
+            #loss_d = discriminator_loss#loss_d.item()
             # loss_s = stft_loss.item()
             # loss_m = mel_loss.item()
             # loss_k = loss_kl_f.item()
