d1c027e14c33967439c72b3338715fdf89a37bc2
FBS <aliu2000@outlook.com>
2023-06-09 21:51:01 +0800

    restore

--- vits_extend/train.py
+++ vits_extend/train.py
@@ -133,10 +133,10 @@ def train(rank, args, chkpt_path, hp, hp_str):
             # Loss
             loss_g = score_loss + feat_loss + mel_loss + stft_loss + loss_kl_f + loss_kl_r * 0.5# + spk_loss * 0.5
 
-            return loss_g, (mutables,fake_audio,ids_slice)
+            return loss_g, mutables
 
         grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
-        (loss,(mutables,fake_audio,ids_slice)), grads = grad_fn(generator_state.params)
+        (loss,mutables), grads = grad_fn(generator_state.params)
 
         # Average across the devices.
         grads = jax.lax.pmean(grads, axis_name='num_devices')
@@ -146,25 +146,23 @@ def train(rank, args, chkpt_path, hp, hp_str):
         new_generator_state = generator_state.apply_gradients(
             grads=grads, batch_stats=mutables['batch_stats'])
     
-        return new_generator_state, loss,fake_audio,ids_slice
+        return new_generator_state, loss#,loss_m,loss_s,loss_k,loss_r
        
     @partial(jax.pmap, axis_name='num_devices')
     def discriminator_step(generator_state:TrainState,
                     discriminator_state: TrainState,
-                    audio_e:jnp.ndarray,
-                    fake_audio_e:jnp.ndarray,
-                    ids_slice_e:jnp.ndarray,
+                    ppg : jnp.ndarray  , pit : jnp.ndarray, spec : jnp.ndarray, spk : jnp.ndarray, ppg_l : jnp.ndarray ,spec_l:jnp.ndarray ,audio_e:jnp.ndarray,
                     key: PRNGKey):
 
         def loss_fn(params):
-            # (fake_audio, ids_slice, z_mask, \
-            # (z_f, z_r, z_p, m_p, logs_p, z_q, m_q, logs_q, logdet_f, logdet_r)),mutables = generator_state.apply_fn(
-            #     {'params': generator_state.params, 'batch_stats': generator_state.batch_stats},
-            #     ppg, pit, spec, spk, ppg_l, spec_l, mutable=['batch_stats'])
-            audio = commons.slice_segments(audio_e, ids_slice_e * hp.data.hop_length, hp.data.segment_size)  # slice
+            (fake_audio, ids_slice, z_mask, \
+            (z_f, z_r, z_p, m_p, logs_p, z_q, m_q, logs_q, logdet_f, logdet_r)),mutables = generator_state.apply_fn(
+                {'params': generator_state.params, 'batch_stats': generator_state.batch_stats},
+                ppg, pit, spec, spk, ppg_l, spec_l, mutable=['batch_stats'])
+            audio = commons.slice_segments(audio_e, ids_slice * hp.data.hop_length, hp.data.segment_size)  # slice
             disc_fake,mutables  = discriminator_state.apply_fn(
                 {'params': params,'batch_stats': discriminator_state.batch_stats},    
-             fake_audio_e, mutable=['batch_stats'])
+             fake_audio, mutable=['batch_stats'])
             disc_real,mutables  = discriminator_state.apply_fn(
                 {'params': params,'batch_stats':  mutables['batch_stats']},
                 audio, mutable=['batch_stats'])
@@ -174,7 +172,7 @@ def train(rank, args, chkpt_path, hp, hp_str):
                 loss_d += jnp.mean((score_fake)**2)
             loss_d = loss_d / len(disc_fake)
           
-            return loss_d,mutables
+            return loss_d,mutables 
         
         # Generate data with the Generator, critique it with the Discriminator.
         grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
@@ -248,11 +246,9 @@ def train(rank, args, chkpt_path, hp, hp_str):
             spec_l = shard(spec_l)
             audio = shard(audio)
             audio_l = shard(audio_l)
-            generator_state, generator_loss,fake_audio,ids_slice= generator_step(generator_state, discriminator_state,ppg=ppg,pit=pit, spk=spk, spec=spec,ppg_l=ppg_l,spec_l=spec_l,audio_e=audio,key=key_generator)
-            fake_audio =shard(fake_audio)
-            ids_slice =shard(ids_slice)
+            generator_state, generator_loss= generator_step(generator_state, discriminator_state,ppg=ppg,pit=pit, spk=spk, spec=spec,ppg_l=ppg_l,spec_l=spec_l,audio_e=audio,key=key_generator)
             #print("Working!2")
-            discriminator_state, discriminator_loss = discriminator_step(generator_state, discriminator_state,ids_slice_e=ids_slice,fake_audio_e=fake_audio,audio_e=audio,key=key_discriminator)
+            discriminator_state, discriminator_loss = discriminator_step(generator_state, discriminator_state,ppg=ppg,pit=pit, spk=spk, spec=spec,ppg_l=ppg_l,spec_l=spec_l,audio_e=audio,key=key_discriminator)
             #print("Working!3")
 
 
