471c5c8c13a0df8cda40a172e4021312f788158b
FBS <aliu2000@outlook.com>
2023-06-10 19:09:00 +0800

    test

--- vits_decoder/mpd.py
+++ vits_decoder/mpd.py
@@ -33,7 +33,7 @@ class DiscriminatorP(nn.Module):
             nn.BatchNorm(use_running_average=False, axis=-1,scale_init=normal_init(0.02)),
             nn.BatchNorm(use_running_average=False, axis=-1,scale_init=normal_init(0.02))
         ]
-        self.conv_post = nn.Conv(features=1, kernel_size=(3, 1), strides=1, padding=(1, 0),kernel_init=normal_init(0.02))
+        self.conv_post = nn.Conv(features=1, kernel_size=(3, 1), strides=1, padding="SAME",kernel_init=normal_init(0.02))
         #self.norm_post = nn.BatchNorm(use_running_average=False, axis=-1,scale_init=normal_init(0.02))
 
     def __call__(self, x):
