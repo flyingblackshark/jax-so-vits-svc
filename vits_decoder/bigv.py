1c113c9971e9b64a89fc92a3284760ad210c358b
FBS <aliu2000@outlook.com>
2023-06-21 11:03:48 +0000

    test

--- vits_decoder/bigv.py
+++ vits_decoder/bigv.py
@@ -5,7 +5,7 @@ from functools import partial
 import flax
 import jax
 from typing import Tuple
-from jax.nn.initializers import normal as normal_init
+
 
 class AMPBlock(nn.Module):
     channels:int
@@ -13,23 +13,23 @@ class AMPBlock(nn.Module):
     dilation:Tuple[int]=(1, 3, 5)
     def setup(self):
         self.convs1 = [
-            nn.Conv(self.channels, [self.kernel_size], 1, kernel_dilation=self.dilation[0],kernel_init=normal_init(0.01)),
-            nn.Conv(self.channels, [self.kernel_size], 1, kernel_dilation=self.dilation[1],kernel_init=normal_init(0.01)),
-            nn.Conv(self.channels, [self.kernel_size], 1, kernel_dilation=self.dilation[2],kernel_init=normal_init(0.01))
+            nn.Conv(self.channels, [self.kernel_size], 1, kernel_dilation=self.dilation[0]),
+            nn.Conv(self.channels, [self.kernel_size], 1, kernel_dilation=self.dilation[1]),
+            nn.Conv(self.channels, [self.kernel_size], 1, kernel_dilation=self.dilation[2])
         ]
-        self.norms1=[nn.BatchNorm(scale_init=normal_init(0.01)) for i in range(3)]
+        self.norms1=[nn.BatchNorm() for i in range(3)]
 
 
         self.convs2 = [
-            nn.Conv(self.channels, [self.kernel_size], 1, kernel_dilation=1,kernel_init=normal_init(0.01)),
-            nn.Conv(self.channels, [self.kernel_size], 1, kernel_dilation=1,kernel_init=normal_init(0.01)),
-            nn.Conv(self.channels, [self.kernel_size], 1, kernel_dilation=1,kernel_init=normal_init(0.01))
+            nn.Conv(self.channels, [self.kernel_size], 1, kernel_dilation=1),
+            nn.Conv(self.channels, [self.kernel_size], 1, kernel_dilation=1),
+            nn.Conv(self.channels, [self.kernel_size], 1, kernel_dilation=1)
         ]
-        self.norms2=[nn.BatchNorm(scale_init=normal_init(0.01)) for i in range(3)]
+        #self.norms2=[nn.BatchNorm() for i in range(3)]
 
 
     def __call__(self, x,train = True):
-        for c1, c2,n1,n2 in zip(self.convs1, self.convs2,self.norms1,self.norms2):
+        for c1, c2,n1 in zip(self.convs1, self.convs2,self.norms1):
            
             #xt = nn.leaky_relu(x, 0.1)
             xt = c1(x.transpose(0,2,1)).transpose(0,2,1)
@@ -41,6 +41,6 @@ class AMPBlock(nn.Module):
             xt = c2(xt.transpose(0,2,1)).transpose(0,2,1)
             xt = nn.leaky_relu(xt, 0.1)
             #xt = commons.snake(xt)
-            xt = n2(xt.transpose(0,2,1),use_running_average=not train).transpose(0,2,1)
+            #xt = n2(xt.transpose(0,2,1),use_running_average=not train).transpose(0,2,1)
             x = xt + x
         return x
