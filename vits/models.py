1136b089ee9a08f703f695d7bd0376449f36b547
FBS <aliu2000@outlook.com>
2023-07-10 06:44:09 +0000

    Add GRL

--- vits/models.py
+++ vits/models.py
@@ -13,7 +13,7 @@ import optax
 from vits import attentions
 from vits import commons
 from vits import modules
-#from vits.modules_grl import SpeakerClassifier
+from vits.modules_grl import SpeakerClassifier
 from vits.utils import f0_to_coarse
 from vits_decoder.generator import Generator
 
@@ -167,10 +167,10 @@ class SynthesizerTrn(nn.Module):
             3,
             0.1
         )
-        # self.speaker_classifier = SpeakerClassifier(
-        #     self.hp.vits.hidden_channels,
-        #     self.hp.vits.spk_dim,
-        # )
+        self.speaker_classifier = SpeakerClassifier(
+            self.hp.vits.hidden_channels,
+            self.hp.vits.spk_dim,
+        )
         self.enc_q = PosteriorEncoder(
             self.spec_channels,
             self.hp.vits.inter_channels,
@@ -208,7 +208,12 @@ class SynthesizerTrn(nn.Module):
         z_f, logdet_f = self.flow(z_q, spec_mask, g=spk,reverse=False,train=train)
         z_r, logdet_r = self.flow(z_p, spec_mask, g=spk,reverse=True,train=train)
 
-        return audio, ids_slice, spec_mask, (z_f, z_r, z_p, m_p, logs_p, z_q, m_q, logs_q, logdet_f, logdet_r)
+        def gradient_reversal(x):
+            zeros = -x + jax.lax.stop_gradient(x)
+            return zeros + jax.lax.stop_gradient(x)
+        
+        spk_preds = self.speaker_classifier(gradient_reversal(x))
+        return audio, ids_slice, spec_mask, (z_f, z_r, z_p, m_p, logs_p, z_q, m_q, logs_q, logdet_f, logdet_r),spk_preds
 
     def infer(self, ppg, pit,vec, spk, ppg_l):
         rng = self.make_rng('rnorms')
--- vits/modules_grl.py
+++ vits/modules_grl.py
@@ -1,77 +1,23 @@
 import jax.numpy as jnp
-from jax import custom_jvp
-
-import flax
 from flax import linen as nn
-from jax.nn.initializers import normal as normal_init
-from flax.training import train_state
-import jax.numpy as jnp
-import jax
-from jax import random
-import numpy as np
-import optax
-from jax import custom_vjp
-# @jax.custom_jvp
-# def gradient_reversal(x):
-#   return x
 
-# @gradient_reversal.defjvp
-# def f_jvp(primals,tangents):
-#     x = primals
-#     x_dot = tangents
-#     primal_out = gradient_reversal(x)
-#     tangent_out = -1 * x_dot * x
-#     return primal_out, tangent_out
-# @jax.custom_vjp
-# def gradient_reversal(x):
-#   return x
+class SpeakerClassifier(nn.Module):
+    embed_dim:int
+    spk_dim:int
+    def setup(self):
+        self.classifier = nn.Sequential([
+            nn.Conv( self.embed_dim, kernel_size=[5]),
+            nn.relu,
+            nn.Conv( self.embed_dim, kernel_size=[5]),
+            nn.relu,
+            nn.Conv( self.spk_dim, kernel_size=[5])]
+        )
 
-# def f_fwd(x):
-#   return gradient_reversal(x), (x)
-
-# def f_bwd(res,g):
-#   x = res
-#   return (g * jnp.ones_like(x) * -1)
-
-# gradient_reversal.defvjp(f_fwd, f_bwd)
-
-# class GradientReversal(nn.Module):
-#     ''' Gradient Reversal Layer
-#             Y. Ganin, V. Lempitsky,
-#             "Unsupervised Domain Adaptation by Backpropagation",
-#             in ICML, 2015.
-#         Forward pass is the identity function
-#         In the backward pass, upstream gradients are multiplied by -lambda (i.e. gradient are reversed)
-#     '''
-
-#     # def __init__(self, lambda_reversal=1):
-#     #     super(GradientReversal, self).__init__()
-#     #     self.lambda_ = lambda_reversal
-
-#     def __call__(self, x):
-#         #return GradientReversalFunction.apply(x, self.lambda_)
-#         return gradient_reversal(x)
-
-
-# class SpeakerClassifier(nn.Module):
-#     embed_dim:int
-#     spk_dim:int
-#     def setup(self):
-#        # super(SpeakerClassifier, self).__init__()
-#         self.classifier = nn.Sequential([
-#             gradient_reversal,
-#             nn.Conv( self.embed_dim, kernel_size=[5]),
-#             nn.relu,
-#             nn.Conv( self.embed_dim, kernel_size=[5]),
-#             nn.relu,
-#             nn.Conv( self.spk_dim, kernel_size=[5])]
-#         )
-
-#     def __call__(self, x):
-#         ''' Forward function of Speaker Classifier:
-#             x = (B, embed_dim, len)
-#         '''
-#         # pass through classifier
-#         outputs = self.classifier(x.transpose(0,2,1)).transpose(0,2,1) # (B, nb_speakers)
-#         outputs = jnp.mean(outputs, axis=-1)
-#         return outputs
+    def __call__(self, x):
+        ''' Forward function of Speaker Classifier:
+            x = (B, embed_dim, len)
+        '''
+        # pass through classifier
+        outputs = self.classifier(x.transpose(0,2,1)).transpose(0,2,1) # (B, nb_speakers)
+        outputs = jnp.mean(outputs, axis=-1)
+        return outputs
