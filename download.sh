15e86ebbbd2a443cd65affc96a0adef952810a67
FBS <aliu2000@outlook.com>
2024-07-30 19:22:31 +0800

    update

---
+++ download.sh
@@ -0,0 +1,3 @@
+cd hubert
+wget https://huggingface.co/TencentGameMate/chinese-hubert-large/resolve/main/pytorch_model.bin
+cd ..
--- instsall.sh
+++ instsall.sh
@@ -1,3 +1,4 @@
 pip install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 pip install -r requirements.txt
+wget https://huggingface.co/TencentGameMate/chinese-hubert-large/resolve/main/pytorch_model.bin?download=true -o ./hubert/pytorch_model.bin
